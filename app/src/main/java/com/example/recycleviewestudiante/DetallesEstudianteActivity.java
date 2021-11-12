450e5eddadd5dfb27a6b5dda6410d5b1376ac7b0
Usuario <Usuario@DESKTOP-0UDCG2J>
2021-11-12 19:35:30 +0100

    Clase 12/11/21

---
+++ app/src/main/java/com/example/recycleviewestudiante/DetallesEstudianteActivity.java
@@ -0,0 +1,14 @@
+package com.example.recycleviewestudiante;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetallesEstudianteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalles_estudiante);
+    }
+}
